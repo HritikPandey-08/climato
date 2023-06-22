c9ae48d0e6e8cb4a18bf5c7552a48195bc910cb5
Hritik Pandey <93441599+HritikPandey-08@users.noreply.github.com>
2023-06-22 16:12:29 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import WeatherReport from "./components/WeatherReport/WeathersReport";
+import WeatherForecast from "./components/WeatherDesc/WeatherForecast";
+import "./styles/global.scss";
+import {
+  WEATHER_API_KEY,
+  CURRENT_WEATHER_URL,
+  FORECAST_WEATHER_URL,
+} from "./utils/api";
 
 function App() {
+  const [currentWeathers, setCurrentWeathers] = useState(null);
+  const [forecast, setForecast] = useState(null);
+  // const [city, //setCity] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  const handleSearchChange = (searchData) => {
+    setSearchValue(searchData);
+    console.log(searchData);
+  };
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      let url;
+      let forecastUrl;
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            //Setting latitude and longitude
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+            console.log(latitude,longitude);
+            //fetchWeatherData(); // Fetch weather data once location is available
+          },
+          (error) => {
+            console.log(error);
+            //fetchWeatherData(); // Fetch weather data even if location permission is denied
+          }
+        );
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+       // fetchWeatherData(); // Fetch weather data even if geolocation is not supported
+      }
+
+      // Fetching weather data based on search value or user's location
+      if (searchValue && searchValue.trim() !== "") {
+        url = `${CURRENT_WEATHER_URL}q=${searchValue}&units=metric&appid=${WEATHER_API_KEY}`;
+        forecastUrl = `${FORECAST_WEATHER_URL}q=${searchValue}&units=metric&appid=${WEATHER_API_KEY}`;
+      } else if (latitude && longitude) {
+        forecastUrl = `${FORECAST_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}`;
+        url = `${CURRENT_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}`;
+      } else {
+        // If neither location nor search value is available, show default page or handle as desired
+        console.log("n")
+        setCurrentWeathers([]);
+        setForecast([]);
+        return
+      }
+
+      try {
+        const weatherResponse = await fetch(url);
+        const forecastResponse = await fetch(forecastUrl);
+        if (!forecastResponse.ok) {
+          // Handle invalid city or error response
+          setForecast([]);
+          //setCity("");
+          return;
+        }
+        if (!weatherResponse.ok) {
+          // Handle invalid city or error response
+          setCurrentWeathers([]);
+          //setCity("");
+          return;
+        }
+        const report = await weatherResponse.json();
+        const forecastreport = await forecastResponse.json();
+        setCurrentWeathers(report);
+        setForecast(forecastreport);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+        // Handle error
+        setCurrentWeathers([]);
+        setForecast([]);
+        //setCity("");
+      }
+    };
+
+    fetchWeatherData();
+  }, [searchValue, latitude, longitude]);
+
+  useEffect(() => {
+    // Log the currentWeathers and forecast values after they are updated
+    console.log("currentWeathers:", currentWeathers);
+    console.log("forecast:", forecast);
+  }, [currentWeathers, forecast]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar onSearchChange={handleSearchChange} />
+    {currentWeathers && <WeatherReport data={currentWeathers} />}
+      {forecast && <WeatherForecast data={forecast} />}
+    </>
   );
 }
 
---
+++ src/components/WeatherDesc/WeatherForecast.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+// import css from "./WeatherDesc.module.scss";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+// import { imagePath } from "../../utils/iconsPath";
+
+function WeatherForecast({data}) {
+  // const [expanded, setExpanded] = useState(false);
+
+  // const handleCardClick = () => {
+  //   setExpanded(!expanded);
+  // };
+
+  const WEEK_DAYS = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  // const WeatherIcon = ({ icon }) => {
+  //   const iconPath = `${imagePath}${icon}.png`;
+  //   return <img src={iconPath} alt={icon} />;
+  // };
+
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+  return (
+    <>
+    {data && data.list && (
+        <Accordion>
+          {data.city.name}
+        {data.list.slice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img src={`icons/${item.weather[0].icon}.png`} className="icon-small" alt="weather" />
+                  <label className="day">{forecastDays[idx]}</label>
+                  <label className="description">{item.weather[0].description}</label>
+                  <label className="description">{item.city}</label>
+                  <label className="min-max">{Math.round(item.main.temp_max)}°C /{Math.round(item.main.temp_min)}°C</label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure:</label>
+                  <label>{item.main.pressure}</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity:</label>
+                  <label>{item.main.humidity}</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds:</label>
+                  <label>{item.clouds.all}%</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind speed:</label>
+                  <label>{item.wind.speed} m/s</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea level:</label>
+                  <label>{item.main.sea_level}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels like:</label>
+                  <label>{item.main.feels_like}°C</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    )}
+    </>
+  );
+}
+
+export default WeatherForecast;
