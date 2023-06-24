378d9c7a7a681035c6e4adbf97c0eac56377ab30
Hritik Pandey <93441599+HritikPandey-08@users.noreply.github.com>
2023-06-24 20:10:56 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import WeatherReport from "./components/WeatherReport/WeathersReport";
@@ -9,100 +9,95 @@ import {
   CURRENT_WEATHER_URL,
   FORECAST_WEATHER_URL,
 } from "./utils/api";
+import WeatherIcon from "./components/WeatherDefaultIcon/WeatherIcon";
 
 function App() {
   const [currentWeathers, setCurrentWeathers] = useState(null);
   const [forecast, setForecast] = useState(null);
-  // const [city, //setCity] = useState("");
   const [searchValue, setSearchValue] = useState("");
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+  const [isLocationOrCityAvailable, setIsLocationOrCityAvailable] =
+    useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessageDesc, setErrorMessageDesc] = useState("");
 
   const handleSearchChange = (searchData) => {
     setSearchValue(searchData);
-    console.log(searchData);
   };
 
   useEffect(() => {
     const fetchWeatherData = async () => {
       let url;
       let forecastUrl;
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            //Setting latitude and longitude
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-            console.log(latitude,longitude);
-            //fetchWeatherData(); // Fetch weather data once location is available
-          },
-          (error) => {
-            console.log(error);
-            //fetchWeatherData(); // Fetch weather data even if location permission is denied
-          }
-        );
-      } else {
-        console.log("Geolocation is not supported by this browser.");
-       // fetchWeatherData(); // Fetch weather data even if geolocation is not supported
-      }
-
-      // Fetching weather data based on search value or user's location
-      if (searchValue && searchValue.trim() !== "") {
-        url = `${CURRENT_WEATHER_URL}q=${searchValue}&units=metric&appid=${WEATHER_API_KEY}`;
-        forecastUrl = `${FORECAST_WEATHER_URL}q=${searchValue}&units=metric&appid=${WEATHER_API_KEY}`;
-      } else if (latitude && longitude) {
-        forecastUrl = `${FORECAST_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}`;
-        url = `${CURRENT_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}`;
-      } else {
-        // If neither location nor search value is available, show default page or handle as desired
-        console.log("n")
-        setCurrentWeathers([]);
-        setForecast([]);
-        return
-      }
 
       try {
+        if (searchValue && searchValue.trim() !== "") {
+          url = `${CURRENT_WEATHER_URL}q=${searchValue}&units=metric&appid=${WEATHER_API_KEY}`;
+          forecastUrl = `${FORECAST_WEATHER_URL}q=${searchValue}&units=metric&appid=${WEATHER_API_KEY}`;
+        } else {
+          const position = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+          });
+
+          const { latitude, longitude } = position.coords;
+          forecastUrl = `${FORECAST_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}`;
+          url = `${CURRENT_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}`;
+        }
+
         const weatherResponse = await fetch(url);
         const forecastResponse = await fetch(forecastUrl);
-        if (!forecastResponse.ok) {
-          // Handle invalid city or error response
-          setForecast([]);
-          //setCity("");
+
+        if (!forecastResponse.ok || !weatherResponse.ok) {
+          setIsLocationOrCityAvailable(false);
+          setErrorMessage("City not found");
+          setErrorMessageDesc("Please enter correct city name.");
+          setForecast(null);
+          setCurrentWeathers(null);
           return;
         }
-        if (!weatherResponse.ok) {
-          // Handle invalid city or error response
-          setCurrentWeathers([]);
-          //setCity("");
-          return;
-        }
+
         const report = await weatherResponse.json();
         const forecastreport = await forecastResponse.json();
+
         setCurrentWeathers(report);
         setForecast(forecastreport);
+        setIsLocationOrCityAvailable(true);
+        setErrorMessage(""); // Reset the error message if successful
+        setErrorMessageDesc("");
       } catch (error) {
         console.error("Error fetching weather data:", error);
-        // Handle error
-        setCurrentWeathers([]);
-        setForecast([]);
-        //setCity("");
+        setIsLocationOrCityAvailable(false);
+        setCurrentWeathers(null);
+        setForecast(null);
+        setErrorMessage("Detecting your location");
+        setErrorMessageDesc(
+          "Your location will be used for real-time weather."
+        );
       }
     };
 
     fetchWeatherData();
-  }, [searchValue, latitude, longitude]);
-
-  useEffect(() => {
-    // Log the currentWeathers and forecast values after they are updated
-    console.log("currentWeathers:", currentWeathers);
-    console.log("forecast:", forecast);
-  }, [currentWeathers, forecast]);
+  }, [searchValue]);
 
   return (
     <>
-      <Navbar onSearchChange={handleSearchChange} />
-    {currentWeathers && <WeatherReport data={currentWeathers} />}
-      {forecast && <WeatherForecast data={forecast} />}
+      <div className="container-fluid px-0">
+        <Navbar onSearchChange={handleSearchChange} />
+        {/* {errorMessage && <div>{errorMessage}</div>} */}
+        {!isLocationOrCityAvailable && (
+          <WeatherIcon
+            errorMessage={errorMessage}
+            errorMessageDesc={errorMessageDesc}
+          />
+        )}
+        <div className="row bg mx-0 px-0">
+          <div className="col-sm-12 col-md-12 col-lg-6 mx-0 px-0">
+            {currentWeathers && <WeatherReport data={currentWeathers} />}
+          </div>
+          <div className="col-sm-12 col-md-12 col-lg-6  mx-0 px-0">
+            {forecast && <WeatherForecast data={forecast} />}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,98 +1,63 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBell,
-  faThLarge,
-  faLocationDot,
-  faMagnifyingGlass,
-} from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import css from "./Navbar.module.scss";
 import "../../styles/toggleButton.css";
-function Navbar({onSearchChange}) {
-
-  const [search , setSearch] = useState("");
+function Navbar({ onSearchChange }) {
+  const [search, setSearch] = useState("");
   const [timeoutId, setTimeoutId] = useState(null);
 
   // handling on change event of search bar
   const handleOnChange = (event) => {
     const searchData = event.target.value;
     setSearch(searchData);
-  
+
     // Clear the previous timeout
     if (timeoutId) {
       clearTimeout(timeoutId);
     }
-  
+
     // Set a new timeout to delay the search
     const newTimeoutId = setTimeout(() => {
       onSearchChange(searchData);
-    }, 600); // Adjust the delay time as needed (e.g., 700 milliseconds)
-  
+    }, 600); // Adjust the delay time as needed (e.g., 600 milliseconds)
+
     setTimeoutId(newTimeoutId);
   };
-  
 
   return (
-    <div className="container-fluid">
-      <div className={`${css.header} padding`}>
-        <div className={css.leftSide}>
-          <div className="menu">
-            <span className={css.icons}>
-              <FontAwesomeIcon icon={faThLarge} />
-            </span>
-          </div>
-          <div className="notification ">
-            <span className={`${css.icons} ${css.rounded}`}>
-              <FontAwesomeIcon icon={faBell} />
-            </span>
-          </div>
-
-          <div className={`${css.location}`}>
-            <span className={`${css.locationIcons} `}>
-              <FontAwesomeIcon icon={faLocationDot} />
-            </span>
-            <div className={css.locationName}>
-              <span className={css.cityName}>Mumbai, </span>
-              <span className={css.countryName}>India</span>
-            </div>
-          </div>
-        </div>
-        <div className={css.center}>
-          {/* Search bar */}
-          <div className="searchbar">
-            <form className="d-flex" role="search">
-              <input
-                className={css.roundedInput}
-                type="search"
-                value={search}
-                onChange={handleOnChange}
-                placeholder="Search city.."
-                aria-label="Search"
-              />
-            {/* Search bar icon */}
-              <button className={css.roundedIcon}>
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </button>
-            </form>
-          </div>
-        </div>
-
-        <div className={css.rightSide}>
-          {/* Dark light mode toggle button */}
-          <div className="mode">
-            <span className="px-2 text-white">
-              <label id="switch" className="switch">
-                <input type="checkbox" id="slider" />
-                <span className="slider round"></span>
-              </label>
-            </span>
-          </div>
-          {/* user icon */}
-          <div className={css.user_account}>
-            <img src="./user.gif" alt="User" className="rounded-circle" />
-          </div>
+    <div className={`${css.header} py-3 px-4`}>
+      <div className={css.center}>
+        {/* Search bar */}
+        <div className="searchbar">
+          <input
+            className={css.roundedInput}
+            type="search"
+            value={search}
+            onChange={handleOnChange}
+            placeholder="Search city.."
+            aria-label="Search"
+          />
+          {/* Search bar icon */}
+          {search === "" && (
+            <button className={css.roundedIcon}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="mx-2" />
+            </button>
+          )}
         </div>
       </div>
+
+      {/* <div className={css.rightSide}>
+        {/* Dark light mode toggle button */}
+        {/* <div className="mode">
+          <span className="px-2 text-white">
+            <label id="switch" className="switch">
+              <input type="checkbox" id="slider" />
+              <span className="slider round"></span>
+            </label>
+          </span>
+        </div>
+      </div> */} 
     </div>
   );
 }
