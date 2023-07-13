9fe077ce39020c896222641e5066b2e8da56c5ca
Hritik Pandey <93441599+HritikPandey-08@users.noreply.github.com>
2023-07-13 21:52:16 +0530

    imagePathUpdate

--- src/components/WeatherDefaultIcon/WeatherIcon.js
+++ src/components/WeatherDefaultIcon/WeatherIcon.js
@@ -1,12 +1,13 @@
 import React from "react";
 import css from "./WeatherIcon.module.scss";
+import { imagePath } from "../../utils/path";
 
 function WeatherIcon(props) {
   return (
     <div className={css.default_message}>
       <div className={css.bgImage}>
         <div className={css.icons}>
-          <img src="icons/weatherIcons.gif" alt="icon" />
+          <img src={imagePath+`weatherIcons.gif`} alt="icon" />
         </div>
         <div className={css.message}>
           <h3>{props.errorMessage}</h3>
